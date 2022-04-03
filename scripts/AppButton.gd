db8c5e61976411aa0229d5c822c38514a31139e8
Jorik Cronenberg <jcronenberg@suse.de>
2022-04-04 00:43:17 +0200

    Add more button functionality
    Icon functionality
    Naming functionality

--- scripts/AppButton.gd
+++ scripts/AppButton.gd
@@ -2,13 +2,50 @@ extends Button
 
 export var app: String
 export var arguments: PoolStringArray
+export var app_name: String
+export var icon_path: String
 export var straight_shell: bool = false
+export var show_app_name: bool = false
 
 func _ready():
+	apply_change()
+
+func apply_change():
 	if !straight_shell:
 		arguments.insert(0, app)
-	#print(arguments)
+	if icon_path:
+		set_logo()
+	elif app_name:
+		text = app_name
+	else:
+		text = app
 
+	if show_app_name:
+		show_name_with_icon()
+	else:
+		show_only_icon()
+
+func set_logo():
+	if icon_path:
+		icon_path = "user://icons/" + icon_path
+		var image = Image.new()
+		image.load(icon_path)
+		var texture = ImageTexture.new()
+		texture.create_from_image(image)
+		$Icon.texture = texture
+
+func show_only_icon():
+	$Icon.margin_bottom = -20
+	$Icon.margin_left = 20
+	$Icon.margin_right = -20
+	$AppName.visible = false
+
+func show_name_with_icon():
+	$Icon.margin_bottom = -50
+	$Icon.margin_left = 35
+	$Icon.margin_right = -35
+	$AppName.text = app_name
+	$AppName.visible = true
 
 func _on_AppButton_pressed():
 	if straight_shell:
