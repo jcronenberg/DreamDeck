d8dd272872cefd2d16b7801fd62cc26c0611602e
Jorik Cronenberg <jcronenberg@suse.de>
2022-06-01 00:27:41 +0200

    Update spotify integration
    This update includes the following:
    - Bundle the sp script with the app
    - Add icons for the SpotifyPanel
    - Create buttons to interact with spotify client

--- scripts/SpotifyPanel.gd
+++ scripts/SpotifyPanel.gd
@@ -2,51 +2,97 @@ extends Control
 
 onready var worker_thread = Thread.new()
 var download_dir_path = OS.get_user_data_dir() + "/cache"
-const refresh_timer = 0.5
-var cum_delta = 0.0
+var script_res_path = "res://custom_resources/sp"
+var script_user_path = OS.get_user_data_dir() + "/scripts/sp"
+var script_dir_path = OS.get_user_data_dir() + "/scripts"
+const metadata_refresh = 1.0
+const state_refresh = 5.0
+var metadata_delta := 0.0
+var state_delta := 5.0 # so we instantly try get state
 var metadata
 var album
 var artist
 var track_name
 var art_url
 var script_path
+const repeat_modes = [
+	"None",
+	"Playlist",
+	"Track"
+	]
+var repeat_textures = [
+	load("res://resources/repeat.tres"),
+	load("res://resources/repeat_selected.tres"),
+	load("res://resources/repeat_1_selected.tres")
+]
+
+# State vars
+var repeat_state: int = 0
+var play_state: bool = false
+var shuffle_state: bool = false
+var volume_state: float = 0.0
 
 onready var config_loader = get_node("/root/ConfigLoader")
 
 func _ready():
+	# Ensure prerequisites exist
+	ensure_dir_exists(download_dir_path)
+	ensure_script_exists()
+
+	# Load config
 	var config_data = config_loader.get_config_data()
 	if config_data.has("spotify_panel"):
 		if config_data["spotify_panel"].has("disabled"):
 			if config_data["spotify_panel"]["disabled"]:
 				queue_free()
-		if config_data["spotify_panel"].has("script_path"):
-			script_path = config_data["spotify_panel"]["script_path"]
-		else:
-			push_warning("Spotify_panel script_path not set in config, disabling the panel")
-			queue_free()
 
-	ensure_download_dir_exists()
+	# Clear cache dir to not fill the user dir with endless albumarts
 	clear_cache()
 
 func _physics_process(delta):
-	cum_delta += delta
-	if (cum_delta <= refresh_timer):
-		return
-	if worker_thread.is_active() and not worker_thread.is_alive():
-		worker_thread.wait_to_finish()
-	if not worker_thread.is_active():
-		worker_thread.start(self, "get_metadata")
-	cum_delta = 0.0
+	metadata_delta += delta
+	state_delta += delta
+	if (state_delta >= state_refresh):
+		state_delta = 0.0
+		if worker_thread.is_active() and not worker_thread.is_alive():
+			worker_thread.wait_to_finish()
+		if not worker_thread.is_active():
+			worker_thread.start(self, "get_state")
+	if (metadata_delta >= metadata_refresh):
+		if worker_thread.is_active() and not worker_thread.is_alive():
+			worker_thread.wait_to_finish()
+		if not worker_thread.is_active():
+			worker_thread.start(self, "get_metadata")
+		metadata_delta = 0.0
+
+func execute_sp_command(command: PoolStringArray):
+	var args = [script_user_path]
+	for c in command:
+		args.append(c)
+	var output = []
+	if OS.execute("bash", args, true, output):
+		push_warning("failed to execute \"sp " + command[0] + "\"")
+
+	return output[0]
+
+func get_state():
+	get_metadata()
+	repeat_state = repeat_modes.find(execute_sp_command(["getloop"]).strip_edges(true, true))
+	play_state = true if execute_sp_command(["getplay"]).strip_edges(true, true) == "Playing" else false
+	shuffle_state = true if execute_sp_command(["getshuffle"]).strip_edges(true, true) == "true" else false
+	volume_state = float(execute_sp_command(["getvolume"]))
+
+	set_state()
+
+func set_state():
+	$Background/Controls/PlayPauseButton.pressed = play_state
+	$Background/Controls/ShuffleButton.pressed = shuffle_state
+	$Background/Controls/RepeatButton.texture_normal = repeat_textures[repeat_state]
 
 func get_metadata():
-	var args = ["metadata"]
-	var output = []
-	if OS.execute("/home/jorik/bin/sp", args, true, output):
-		push_warning("Couldn't get spotify metadata")
-		return
-
-	if metadata != output[0]:
-		set_metadata(output[0])
+	var output = execute_sp_command(["metadata"])
+	if metadata != output:
+		set_metadata(output)
 
 func set_metadata(new_metadata):
 	metadata = new_metadata
@@ -81,19 +127,37 @@ func download_cover():
 		return
 	change_cover(filename)
 
-func change_cover(filename):
-	var complete_cover_path = "user://cache/" + filename
+func create_texture_from_image(image_path):
 	var image = Image.new()
-	image.load(complete_cover_path)
+	image.load(image_path)
 	var texture = ImageTexture.new()
 	texture.create_from_image(image)
-	$Background/AlbumArt.texture = texture
+	return texture
 
-func ensure_download_dir_exists():
+func change_cover(filename):
+	var complete_cover_path = "user://cache/" + filename
+	$Background/AlbumArt.texture = create_texture_from_image(complete_cover_path)
+
+func ensure_dir_exists(path):
 	var dir = Directory.new()
-	if dir.open(download_dir_path) != OK:
-		if dir.make_dir(download_dir_path) != OK:
-			push_warning("Couldn't create cache dir")
+	if dir.open(path) != OK:
+		if dir.make_dir(path) != OK:
+			push_warning("Couldn't create " + path + " dir")
+
+func check_file_exists(path) -> bool:
+	var dir = Directory.new()
+	if dir.file_exists(path):
+		return true
+	return false
+
+func ensure_script_exists():
+	ensure_dir_exists(script_dir_path)
+	if not check_file_exists(script_user_path):
+		var res_f = File.new()
+		var user_f = File.new()
+		res_f.open(script_res_path, File.READ)
+		user_f.open(script_user_path, File.WRITE)
+		user_f.store_buffer(res_f.get_buffer(res_f.get_len()))
 
 func clear_cache():
 	var dir = Directory.new()
@@ -106,3 +170,36 @@ func clear_cache():
 			file_name = dir.get_next()
 	else:
 		push_warning("An error occurred when trying to access the path.")
+
+
+func _on_PlayPauseButton_pressed():
+	execute_sp_command(["play"])
+
+
+func _on_SkipBackButton_pressed():
+	execute_sp_command(["prev"])
+
+
+func _on_SkipForwardButton_pressed():
+	execute_sp_command(["next"])
+
+
+func _on_RepeatButton_pressed():
+	repeat_state += 1
+	if repeat_state >= len(repeat_modes):
+		repeat_state = 0
+	$Background/Controls/RepeatButton.texture_normal = repeat_textures[repeat_state]
+	execute_sp_command(["setloop", repeat_modes[repeat_state]])
+
+
+func _on_ShuffleButton_pressed():
+	shuffle_state = !shuffle_state
+	execute_sp_command(["setshuffle", str(shuffle_state).to_lower()])
+
+
+func _on_VolumeDownButton_pressed():
+	execute_sp_command(["setrelvolume", "-5"])
+
+
+func _on_VolumeUpButton_pressed():
+	execute_sp_command(["setrelvolume", "+5"])
