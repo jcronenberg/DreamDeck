2a8cbbbaec27f558a6d50be4ba0c9670b3b2ab5a
Jorik Cronenberg <jcronenberg@suse.de>
2022-08-08 11:41:51 +0200

    Remove internal config completely

--- scripts/ConfigLoader.gd
+++ scripts/ConfigLoader.gd
@@ -1,21 +1,14 @@
 extends Node
 
 var data
-var internal_data
 
 const CONFIG_PATH = "user://config.json"
-const INTERNAL_CONFIG_PATH = "user://internal/internal_config.json"
 
 func _ready():
 	data = load_config(CONFIG_PATH)
 	if not data:
 		push_error("Couldn't load config file")
 		get_tree().quit()
-
-	ensure_dir_exists("user://internal")
-	internal_data = load_config(INTERNAL_CONFIG_PATH)
-	if not internal_data:
-		push_error("Couldn't load internal config file")
 
 
 # Load config at path. If it doesn't exist this create's it
@@ -75,23 +68,6 @@ func save_plugin_config(name: String, new_data) -> bool:
 
 func get_config_data():
 	return data
-
-
-func get_internal_config_data():
-	return internal_data
-
-
-# Save the provided data as the internal config
-# Callers should make sure they don't overwrite existing data unless they want to
-func save_internal_config(new_data):
-	var config_file = File.new()
-
-	ensure_dir_exists("user://internal")
-
-	if config_file.open(INTERNAL_CONFIG_PATH, File.WRITE) != OK:
-		push_error("Couldn't create file at " + INTERNAL_CONFIG_PATH)
-		return false
-	config_file.store_line(to_json(new_data))
 
 
 func ensure_dir_exists(path):
