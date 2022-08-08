5d119ed944420774431b9f15a11ba2603b9bf121
Jorik Cronenberg <jcronenberg@suse.de>
2022-08-08 22:33:07 +0200

    Switch Macroboard to plugin config system

--- plugins/Macroboard/scripts/Macroboard.gd
+++ plugins/Macroboard/scripts/Macroboard.gd
@@ -1,29 +1,25 @@
 extends Control
 
+# Constants
+const PLUGIN_NAME = "Macroboard"
+
+# Global nodes
 onready var config_loader = get_node("/root/ConfigLoader")
 
 func _ready():
 	load_from_config()
-	#debug
-	#var save_state = save()
-	#var config_file = File.new()
-	#config_file.open("user://config.json", File.READ_WRITE)
-	#print(JSON.print(save_state, "\t"))
-	#config_file.store_string(JSON.print(save_state, "\t"))
-	#var output_json = JSON.parse(config_file.get_as_text())
-	#print(output_json.result)
 
 func load_from_config():
 	var macro_row = load("res://plugins/Macroboard/scenes/MacroRow.tscn")
 	var app_button = load("res://plugins/Macroboard/scenes/AppButton.tscn")
-	var data = config_loader.get_config_data()
+	var data = config_loader.get_plugin_config(PLUGIN_NAME)
 
 	# Load button settings
-	var button_min_size = Vector2(data["macroboard"]["button_settings"]["height"], data["macroboard"]["button_settings"]["width"])
+	var button_min_size = Vector2(data["button_settings"]["height"], data["button_settings"]["width"])
 
 	# Load buttons
 	# Iterate through rows in data
-	for row in data["macroboard"]["layout"].values():
+	for row in data["layout"].values():
 		# Instance row so we can modify it
 		var cur_row = macro_row.instance()
 
@@ -48,16 +44,14 @@ func load_from_config():
 
 func save():
 	var save_dict = {
-		"macroboard" : {
-			"layout" : {}
-		}
+		"layout" : {}
 	}
 	var row_count = 0
 	for rows in $RowSeparator.get_children():
-		save_dict["macroboard"]["layout"]["row" + str(row_count)] = {}
+		save_dict["layout"]["row" + str(row_count)] = {}
 		var button_count = 0
 		for button in rows.get_children():
-			save_dict["macroboard"]["layout"]["row" + str(row_count)]["button" + str(button_count)] = button.save()
+			save_dict["layout"]["row" + str(row_count)]["button" + str(button_count)] = button.save()
 			button_count += 1
 		row_count += 1
 	return save_dict
