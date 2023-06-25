e2fcad70da5387be5109d5639b48d0b604a9926e
Jorik Cronenberg <jcronenberg@suse.de>
2023-06-26 01:01:06 +0200

    Only allow SpotifyPanelLegacy on linux again

--- plugins/SpotifyPanelLegacy/Loader.gd
+++ plugins/SpotifyPanelLegacy/Loader.gd
@@ -4,6 +4,9 @@ var loaded := false
 
 
 func load(node):
+	if OS.get_name() != "X11":
+		push_error("SpotifyPanelLegacy only works on linux")
+		return
 	if not loaded:
 		node.get_node("/root/Main/VSeparator/ElementSeparator/SpotifyMargin").add_child(
 			load("res://plugins/SpotifyPanelLegacy/scenes/SpotifyPanelLegacy.tscn").instance()
