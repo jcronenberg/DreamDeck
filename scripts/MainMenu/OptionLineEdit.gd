d38c03cb01c8dea03d902c921e0b057999fde8cd
Jorik Cronenberg <jcronenberg@suse.de>
2023-07-04 21:38:22 +0200

    OptionLineEdit: Return integer instead of float
    Even though json doesn't handle int, this still makes it easier to compare the
    returned dicts inside the app.

--- scripts/MainMenu/OptionLineEdit.gd
+++ scripts/MainMenu/OptionLineEdit.gd
@@ -19,8 +19,7 @@ func return_value():
 	match type:
 		TYPE_INT:
 			if ret_val.is_valid_integer():
-				# Because json doesn't handle int
-				return ret_val.to_float()
+				return ret_val.to_int()
 		TYPE_REAL:
 			if ret_val.is_valid_float():
 				return ret_val.to_float()
