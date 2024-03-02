6cec91e2b76c8d1784cedce0b50650e494a90750
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2024-03-02 15:00:37 +0100

    SSH: Remove leftover code from loader

--- plugins/ssh/loader.gd
+++ plugins/ssh/loader.gd
@@ -5,23 +5,6 @@ func _init():
 	plugin_name = "ssh"
 	scene = "res://plugins/ssh/scripts/ssh_controller.gd"
 
-# var loaded := false
-# var controller_node
-
-
-# func plugin_load():
-# 	if not loaded:
-# 		controller_node = load("res://plugins/ssh/scripts/ssh_controller.gd").new()
-# 		controller_node.name = "SSHController"
-# 		add_child(controller_node)
-# 		loaded = true
-
-
-# func plugin_unload():
-# 	if loaded:
-# 		controller_node.queue_free()
-# 		loaded = false
-
 
 func get_controller():
 	if _loaded:
