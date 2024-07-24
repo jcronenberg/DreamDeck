ca3d6c8ac99e802647d0443717c5ffff6f786f69
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2024-07-25 00:15:12 +0200

    Improve user facing spotify panel scene name

--- plugins/spotify_panel/loader.gd
+++ plugins/spotify_panel/loader.gd
@@ -2,5 +2,5 @@ class_name SpotifyPanelPluginLoader
 extends PluginLoaderBase
 
 func _init():
-	scenes = {"SpotifyPanel": "res://plugins/spotify_panel/scenes/spotify_panel.tscn"}
 	plugin_name = "Spotify Panel"
+	scenes = {"Spotify Panel": "res://plugins/spotify_panel/scenes/spotify_panel.tscn"}
