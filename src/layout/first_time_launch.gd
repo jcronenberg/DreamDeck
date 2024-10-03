8bb1ba565b30ab144e85e821ba79228e66322f85
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2024-10-04 00:35:45 +0200

    Switch first time launch helper to PopupManager

--- src/layout/first_time_launch.gd
+++ src/layout/first_time_launch.gd
@@ -2,5 +2,7 @@ extends CenterContainer
 
 
 func _on_button_pressed():
-	get_node("/root/Main/LayoutPopup").new_panel()
+	var panel_editor: PanelEditor = PanelEditor.new()
+	panel_editor.show_new_panel()
+	PopupManager.init_popup(panel_editor, panel_editor.save, func not_required(__) -> void: pass)
 	GlobalSignals.enter_edit_mode()
