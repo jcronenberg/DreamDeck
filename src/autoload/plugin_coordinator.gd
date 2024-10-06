8356db9b243fce9b95d5201b0d38143f94ca8c65
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2024-10-07 00:05:57 +0200

    Fix leftover additional arguments when calling PopupManager

--- src/autoload/plugin_coordinator.gd
+++ src/autoload/plugin_coordinator.gd
@@ -206,7 +206,7 @@ func remove_scene(plugin_name: String, scene: String):
 func edit_panel(panel: LayoutPanel):
 	var panel_editor: PanelEditor = PanelEditor.new()
 	panel_editor.show_panel_config(panel.get_plugin_instance().edit_config())
-	PopupManager.init_popup(panel_editor, panel_editor.save, func not_required(__) -> void: pass)
+	PopupManager.init_popup(panel_editor, panel_editor.save, func unused() -> void: pass)
 
 
 func generate_plugins_enum() -> Dictionary:
@@ -245,7 +245,7 @@ func add_panel(leaf: DockableLayoutPanel):
 	get_node("/root/Main/Layout").set_new_panel_leaf(leaf)
 	var panel_editor: PanelEditor = PanelEditor.new()
 	panel_editor.show_new_panel()
-	PopupManager.init_popup(panel_editor, panel_editor.save, func not_required(__) -> void: pass)
+	PopupManager.init_popup(panel_editor, panel_editor.save, func unused() -> void: pass)
 
 
 class Plugin:
--- src/layout/first_time_launch.gd
+++ src/layout/first_time_launch.gd
@@ -4,5 +4,5 @@ extends CenterContainer
 func _on_button_pressed():
 	var panel_editor: PanelEditor = PanelEditor.new()
 	panel_editor.show_new_panel()
-	PopupManager.init_popup(panel_editor, panel_editor.save, func not_required(__) -> void: pass)
+	PopupManager.init_popup(panel_editor, panel_editor.save, func unused() -> void: pass)
 	GlobalSignals.enter_edit_mode()
